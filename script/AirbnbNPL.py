9598580c75cb36af6fd0b0eac870e1d8534635a5
admin <yc.qingfeng@gmail.com>
2019-06-21 17:06:30 +0800

    init

--- script/AirbnbNPL.py
+++ script/AirbnbNPL.py
@@ -8,55 +8,30 @@ import re
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-
-def sort_by_value(d):
-    items = d.items()
-    backitems = [[v[1], v[0]] for v in items]
-    backitems.sort()
-    return [backitems[i][1] for i in range(0, len(backitems))]
-
-
 client = pymongo.MongoClient('mongodb://yc:yc@ycsin.cn:27017')
 db = client['data']
+
 dataset = db['airbnb'].find()
 
-file = open('words', 'w')
-
-words_frequency = {}
-
 for data in dataset:
-    if "location" in data.keys():
+    if ("location" in data.keys()) and ("tags" not in data.keys()):
         location = data['location'].encode('utf8')
         if location == 'None':
             continue
-        file.write(location)
-        file.write('\r')
 
         words_list = jieba.posseg.cut(location)
-        for word, flag in words_list:
-            key = '%s[%s]' % (word, flag)
-            if key not in words_frequency.keys():
-                words_frequency[key] = 1
-            else:
-                words_frequency[key] += 1
+        tags = []
+        for segment, flag in words_list:
+            if flag in ['uj', 'p', 'c', 'm', 'd', 'x', 'r', 'ul', 'u', 'eng']:  # 过滤部分词性
+                continue
+            if segment in ['是', '有', 'the', '可', '请']:
+                continue
 
-file.flush()
-file.close()
+            tags.append({
+                'flag': flag,
+                'segment': segment
+            })
 
-file = open('words_freq', 'w')
-list = sorted(words_frequency.items(), key=lambda d: d[1], reverse=True)
-
-for item in list:
-    flag = re.findall('\[(.*)\]', item[0])[0]
-    if flag in ['uj', 'p', 'c', 'm', 'd', 'x', 'r', 'ul', 'u', 'eng']:  # 过滤部分词性
-        continue
-    flag = re.findall('^(.*)\[.*\]', item[0])[0]
-    if flag in ['是', '有', 'the', '可', '请']:
-        continue
-    file.write('%s : %s' % (item[0], item[1]))
-    file.write('\r')
-
-file.flush()
-file.close()
+        db['airbnb'].update_one({'id': data['id']}, {'$set': {'tags': tags}})
 
 # print ' '.join(seg_list)
